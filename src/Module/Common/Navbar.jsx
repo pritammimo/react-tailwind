427deac77b69a3cfc06d25463d1645c3d9cf28c8
pritamcn <crpritamsaha@gmail.com>
2023-04-24 22:15:54 +0530

    notification

--- src/Module/Common/Navbar.jsx
+++ src/Module/Common/Navbar.jsx
@@ -10,12 +10,19 @@ const Navbar = () => {
   </div>
   <div className="flex-none gap-2">
     <div>
+    <div className="dropdown dropdown-end">
     <button className="btn btn-ghost btn-circle">
       <div className="indicator">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
         <span className="badge badge-xs badge-primary indicator-item"></span>
       </div>
     </button>
+        <ul tabIndex={0} className="menu dropdown-content menu-compact lg:menu-normal bg-base-100 w-56 p-2 rounded-box">
+          <li><a>Item 1</a></li> 
+          <li><a>Item 2</a></li>
+        </ul>
+      </div>
+  
     </div>
     <div class="dropdown dropdown-end">
       <label tabindex="0" class="btn btn-ghost btn-circle">
